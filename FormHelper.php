98cf461cb0654429f3d14deeac6fe068c4d7f4bb
Vladislav Sarosek <91774585+Sararun@users.noreply.github.com>
2022-07-27 04:20:12 +0700

    Update FormHelper.php

--- FormHelper.php
+++ FormHelper.php
@@ -1,119 +1,60 @@
 <?php
-class FormHelper {
-    protected $values = array();
-    public function __construct($values = array()) {
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $this->values = $_POST;
-        } else {
-            $this->values = $values;
+require 'FormHelper.php';
+$operand=array('+','-','*','/');
+// Если форма передана обработку проверить достоверность данных
+
+    if($_SERVER['REQUEST_METHOD']=='POST'){
+        list($errors, $input)=validateForm();
+        if($errors){
+            showForm($errors);
+        }else{
+            processForm($input);
+            showForm();
         }
+    }else{
+        showForm();
     }
 
-    public function input($type, $attributes = array(), //Метод для формирования разметки
-                          $isMultiple = false) {
-        $attributes['type'] = $type;
-        if (($type == 'radio') || ($type == 'checkbox')) {
-            if ($this->isOptionSelected($attributes['name']
-                ?? null, $attributes['value'] ?? null)) {
-                $attributes['checked'] = true;
-            }
-        }
-        return $this->tag('input', $attributes, $isMultiple);
-    }
 
-    public function select($options, $attributes = array()) { //Рахметка для дикскрипторов в стиле select
-        $multiple = $attributes['multiple'] ?? false;
-        return
-            $this->start('select', $attributes, $multiple) .
-            $this->options($attributes['name'] ?? null, $options) .
-            $this->end('select');
-    }
-
-    public function textarea($attributes = array()) { //Метод формированяия текста
-        $name = $attributes['name'] ?? null;
-        $value = $this->values[$name] ?? '';
-        return $this->start('textarea', $attributes) .
-            htmlentities($value) .
-            $this->end('textarea');
-    }
-
-    public function tag($tag, $attributes = array(), //Метод закрывающий разметку </input>
-                        $isMultiple = false) {
-        return
-            "<$tag {$this->attributes($attributes, $isMultiple)} />";
-    }
-
-    public function start($tag, $attributes = array(),  //ФОрмирования начального диксриптора формы
-                          $isMultiple = false) {
-/* Дескрипторы <select> и <textarea> не получают
- атрибуты value*/
-        $valueAttribute =
-            (! (($tag == 'select')||($tag == 'textarea')));
-        $attrs = $this->attributes($attributes, $isMultiple,
-            $valueAttribute);
-        return "<$tag $attrs>";
-    }
-    public function end($tag) { //Формирования конечного дискриптора формы
-        return "</$tag>";
-    }
-    protected function attributes($attributes, $isMultiple, $valueAttribute = true) {   //Формиорвание атрибутов
-        $tmp = array();
-
-        /* Если данный дескриптор может содержать атрибут value,
-         а его имени соответствует элемент в массиве значений,
-         то установить этот атрибут*/
-        if ($valueAttribute && isset($attributes['name'])
-            && array_key_exists($attributes['name'],
-                $this->values)) {
-            $attributes['value'] =
-                $this->values[$attributes['name']];
-        }
-        foreach ($attributes as $k => $v) {
-        /* Истинное логическое значение означает
-         логический атрибут*/
-            if (is_bool($v)) {
-                if ($v) { $tmp[] = $this->encode($k); }
-            }
-        // иначе k = v
-            else {
-                $value = $this->encode($v);
-        /* Если это многозначный элемент, присоединить
-         квадратные скобки ([]) к его имени*/
-                if ($isMultiple && ($k == 'name')) {
-                    $value .= '[]';
-                }
-                $tmp[] = "$k=\"$value\"";
-            }
-        }
-        return implode(' ', $tmp);
-        }
-
-    protected function options($name, $options) { // Форматирование options для select
-        $tmp = array();
-        foreach($options as $k => $v){
-            $s = "<option value=\"{$this->encode($k)}\"";
-            if($this->isOptionSelected($name, $k)){
-                $s .=' selected';
-            }
-            $s .=">{$this->encode($v)}</option>";
-            $tmp[] = $s;
-        }
-        return implode('', $tmp);
-    }
-
-    protected function isOptionSelected($name, $value){    //Реакция на нажатие в select
-        if(!isset($this->values[$name])){
-            return false;
-        }elseif (is_array($this->values[$name])){
-            return in_array($value, $this->values[$name]);
-        }
-        else{
-            return $value == $this->values[$name];
-        }
-    }
-
-    public function encode($s){
-        return htmlentities($s);
-    }
+function showForm($errors= array()){   //  показ формы
+    $defaults= array('num1'=>2, 'secArgument'=>2, 'num2'=>8);
+    $form=new FormHelper($defaults);
+    include 'math-form.php';    //  html разметка
 }
+function validateForm(){   //  проверка качества формы
+    $input=array();
+    $errors=array();
+    $input['secArgument']=$GLOBALS['operand'][$_POST['secArgument']]??'';
+    if(!in_array($input['ap'], $GLOBALS['operand'])){
+        $errors='Please select a valid operation';
+    }
+    $input['num1']=filter_input(INPUT_POST, 'num1',FILTER_VALIDATE_FLOAT ); //  ключи должны быть числовыми
+    if(is_null($input['num1']) || ($input['num1']===false)){
+        $errors[]='Please check valid first number';
+    }
+    $input['num2']=filter_input(INPUT_POST, 'num2',FILTER_VALIDATE_FLOAT);
+    if(is_null($input['num2']) || ($input['num2']===false)){
+        $errors[]='Please check valid second number';
+    }
+    if(($input['secArgument'] == '/') && ($input['num2']==0)){   //  деление на ноль
+        $errors[]='Please don\'division by zero';
+    }
+    return array($errors, $input);
+}
+
+function processForm($input){
+   $result=0;
+   if($input['secArgument']=='+'){
+       $result= $input['num1'] + $input['num2'];
+   }elseif ($input['secArgument']=='-'){
+       $result= $input['num1'] - $input['num2'];
+   }elseif($input['secArgument']=='*'){
+       $result= $input['num1'] * $input['num2'];
+   }elseif($input['secArgument']=='/'){
+       $result= $input['num1'] / $input['num2'];
+   }
+   $message = "{$input['num1']} {$input['secArgument']}  {$input['num2']}=$result";
+   print "<h3>    $message  </h3>";
+}
+
 
